08705903d93052ccf9a9dedbc605e4f966ba8c7d
minhph <hoangminh124@gmail.com>
2023-02-21 21:08:55 +0700

    Math practice

--- practice/main.go
+++ practice/main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"math"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	input, err1 := reader.ReadString('\n')
-	fmt.Println("You entered:", input)
+	i1, i2, i3 := 22, 45, 59
+	intSum := i1 + i2 + i3
+	fmt.Println(intSum)
 
-	if err1 != nil {
-		fmt.Println(err1)
-	} else {
-		fmt.Println("no error")
-	}
+	f1, f2, f3 := 22.34, 421.24, 120.31
+	floatSum := f1 + f2 + f3
+	fmt.Println(floatSum)
 
-	fmt.Print("Enter a number: ")
-	numInput, _ := reader.ReadString('\n')
-	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Value of number:", aFloat)
-	}
-
-	// reader := bufio.NewReader(os.Stdin): This creates a new buffered reader that reads
-	// from the standard input (i.e. the console).
-	// The bufio package is used for buffered input/output operations,
-	// which can be more efficient than reading or writing one byte at a time.
-
+	floatSum = math.Round(floatSum)
+	fmt.Println("The sum is now: ", floatSum)
 }
