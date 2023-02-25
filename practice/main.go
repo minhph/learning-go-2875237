c2dffaffb13adbc216885edf9eed8e4ffa01d1ba
minhph <hoangminh124@gmail.com>
2023-02-25 18:19:00 +0700

    Pointer explanation.

--- practice/main.go
+++ practice/main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Value 1: ")
-	stringInput1, _ := reader.ReadString('\n')
-	floatOperator1, err1 := strconv.ParseFloat(strings.TrimSpace(stringInput1), 64)
-
-	if err1 != nil {
-		fmt.Println(err1)
-		panic("The input value must be a number.")
-	}
-
-	fmt.Print("Value 2: ")
-	stringInput2, _ := reader.ReadString('\n')
-	floatOperator2, err2 := strconv.ParseFloat(strings.TrimSpace(stringInput2), 64)
-
-	if err2 != nil {
-		fmt.Println(err2)
-		panic("The input value must be a number.")
-	}
-
-	sum := floatOperator1 + floatOperator2
-	fmt.Printf("The sum of %.2f and %.2f is %.2f\n", floatOperator1, floatOperator2, sum)
-
-	// The ReadString method actually invokes standard input (console).
-	// The NewReader(os.Stdin) method only return an reader object that can read from standard input.
-	// It does not invoke input.
-	// Therefore, we only need 1 reader for this.
-	// The argument "\n" on reader.ReadString('\n') means saves characters on buffer memory until the delimiter "\n" is reached.
-	// strconv.ParseFloat is used to parse a string value into Float format, return output float and a error message.
-	// Panic is used to terminate program if exception happens, with a message as argument.
+	anInt := 2
+	var p *int // a pointer pointing to an int value, zero-value: nil
+	p = &anInt // assign address of anInt to p (type stricted)
+	fmt.Println(*p)
 }
