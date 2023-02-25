a1a6d2ec1547a1b2cf8e8c01a2012613b0946b12
minhph <hoangminh124@gmail.com>
2023-02-25 18:20:52 +0700

    Pointer explanation.

--- practice/main.go
+++ practice/main.go
@@ -6,7 +6,9 @@ import (
 
 func main() {
 	anInt := 2
-	var p *int // a pointer pointing to an int value, zero-value: nil
-	p = &anInt // assign address of anInt to p (type stricted)
-	fmt.Println(*p)
+	var p *int      // a pointer pointing to an int value, zero-value: nil
+	p = &anInt      // assign address of anInt to p (type stricted)
+	fmt.Println(p)  // the memory address which pointer is pointing to
+	fmt.Println(*p) // the value stored at the memory address the pointer is pointing to
+	fmt.Println(&p) // the memory address of the pointer itself
 }
