4b990b36f14a946ba2aca1ee89c2f8a7f1d04ee6
minhph <hoangminh124@gmail.com>
2023-02-26 19:25:14 +0700

    Different kinds of function.

--- practice/main.go
+++ practice/main.go
@@ -5,12 +5,25 @@ import (
 )
 
 func main() {
-	// This is a struct
-	type Hero struct {
-		FirstSkill  string
-		SecondSkill string
-	}
+	printSomething()
+	sumTwoValues := addTwoValues(3, 4)
+	sumManyValues := addManyValues(84, 512, 53)
+	fmt.Println(sumTwoValues)
+	fmt.Println(sumManyValues)
+}
 
-	rouie := Hero{"Holy Emblem", "Holy Gate"}
-	fmt.Println(rouie)
+func printSomething() {
+	fmt.Println("Print something.")
+}
+
+func addTwoValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addManyValues(values ...int) int {
+	total := 0
+	for v := range values {
+		total = total + values[v]
+	}
+	return total
 }
