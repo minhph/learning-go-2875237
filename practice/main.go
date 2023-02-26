fbe99ba51b3746eb5bd460d3e4cf24e48e360007
minhph <hoangminh124@gmail.com>
2023-02-26 17:58:25 +0700

    Custom type.

--- practice/main.go
+++ practice/main.go
@@ -2,47 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	states := make(map[string]string)
-	// states holds a data struct consisiting of a hexadecimal
-	// memory address of the object on heap, an integer (size), an integer(capacity)
-
-	fmt.Println(states)
-	states["WA"] = "Washington"
-	states["NY"] = "New York"
-	states["OL"] = "Oklahoma"
-	states["MI"] = "Missisipi"
-	fmt.Println(states)
-	fmt.Printf("%p\n", states)
-
-	oklahoma := states["OL"]
-	fmt.Println(oklahoma)
-
-	delete(states, "NY")
-	fmt.Println(states)
-
-	for k, v := range states {
-		fmt.Printf("%v: %v\n", k, v)
+	// This is a struct
+	type Hero struct {
+		FirstSkill  string
+		SecondSkill string
 	}
 
-	// for...range is used for iteration, range is always used with for
-	// this returns an index first, then a value one at a time
-
-	keys := make([]string, len(states))
-	fmt.Println(len(keys))
-
-	i := 0
-	for k := range states {
-		keys[i] = k
-		i++
-	}
-	fmt.Println(keys)
-	sort.Strings(keys)
-
-	for i := range keys {
-		fmt.Println(states[keys[i]])
-	}
+	rouie := Hero{"Holy Emblem", "Holy Gate"}
+	fmt.Println(rouie)
 }
